fdc9cb188eea02edfc4e6a8c3a496de3ea437cc7
RickApple <74308315+RickApple@users.noreply.github.com>
2025-04-23 14:33:31 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, CMake!\n");
+    #if defined(_WIN32)
+        printf("Hello, windows!\n");
+    #elif defined(APPLE)
+        printf("Hello, apple!\n");
+    #else
+        printf("Linux"\n);
+    #endif
     return 0;
 }
